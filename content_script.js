58d384ce82a820c3d94f212c5381d46ca8871e1a
Julienedies <Julienedies@gmail.com>
2014-02-01 20:15:21 +0800

    custom saving element

--- content_script.js
+++ content_script.js
@@ -1,18 +1,37 @@
 //chrome.downloads.download({url:location.href,filename:'test.txt'}, function(){});
 function $(selector){
-	return document.querySelector(selector);
+	return selector ? document.querySelector(selector) : undefined;
 } 
-var url = location.href;
-var filename = $('title').innerText;
-var content = $('td.postcontent') && $('td.postcontent').innerText || $('body').innerText;
-
-var dobj = {url : url, filename : filename, content : content};
 
 
-//传递数据给popup.html
-chrome.extension.sendRequest(dobj, function(response) {
-	  console.log(response.farewell);
-});
+// 根据popup传过来的selector选择文本，进行保存
+chrome.extension.onRequest.addListener( 
+		  function(request, sender, sendResponse) {
+		    console.log(sender.tab ?
+		                "from a content script:" + sender.tab.url :
+		                "from the extension");
+		    
+		    var selector = request.selector;
+		    var url = location.href;
+		    var filename = $('title').innerText;
+		    var content = $(selector) && $(selector).innerText || $('td.postcontent') && $('td.postcontent').innerText || $('body').innerText;
+
+		    var dobj = {url : url, filename : filename, content : content};
+		    
+		    //sendResponse(dobj);
+		    
+		   //传递选择的文本数据给popup.html
+		    chrome.extension.sendRequest(dobj, function(response) {
+		    	  console.log(response.farewell);
+		    });
+		    
+		  }
+);
+
+
+
+
+
 
 
 
--- popup.js
+++ popup.js
@@ -1,37 +1,63 @@
 // svg实现的loading图形
 var loadingHtml = '<span style="margin:5px auto;display:inline-block;width:100%;text-align:center;" name="_loading_"><svg width="16" height="16" viewBox="0 0 300 300" xmlns="http://www.w3.org/2000/svg" version="1.1"><path d="M 150,0 a 150,150 0 0,1 106.066,256.066 l -35.355,-35.355 a -100,-100 0 0,0 -70.711,-170.711 z" fill="#3d7fe6"><animateTransform attributeName="transform" attributeType="XML" type="rotate" from="0 150 150" to="360 150 150" begin="0s" dur="1s" fill="freeze" repeatCount="indefinite" /></path></svg></span>';
 
-
-
-function clickHandler(e) {
-	chrome.tabs.executeScript(null, {file: "content_script.js"});
-	/*
-	chrome.tabs.executeScript(null, {
-		code : "document.body.bgColor='red'"
-	});
-	*/
-}
-
 /*
 document.addEventListener('DOMContentLoaded', function () {
     document.querySelector('button').addEventListener('click', clickHandler);
   });
 */
 
+//向当前html注入js
+chrome.tabs.executeScript(null, {file: "content_script.js"});
+
 
 //init
 $(function($){
 	
+	var $select = $('#select');
 	var $url = $('#url');
 	var $filename = $('#filename');
 	var $content = $('#content');
 	var $save = $('#save');
 	
 	
-	
 	$('#saveTotxt').click(function(){
-		chrome.tabs.executeScript(null, {file: "content_script.js"});
+		var selector = $select.val();
+		
+		//发消息给content scripts
+		chrome.tabs.getSelected(null, function(tab) {
+			  chrome.tabs.sendRequest(tab.id, {selector:selector}, function(response) {
+			    console.log(response);
+			  });
+			});		
+		
 	});
+	
+	
+	// 接收content script发过来的消息
+	chrome.extension.onRequest.addListener(
+			  function(request, sender, sendResponse) { 
+			    console.log(sender.tab ?
+			                "from a content script:" + sender.tab.url :
+			                "from the extension");
+			    if (request.greeting == "hello"){
+			    	sendResponse({farewell: "goodbye"});
+			    }
+			    else{
+			    	sendResponse({}); // snub them.
+			    }
+			      
+			    var url = request.url;
+			    var filename = request.filename;
+			    var content = request.content;
+			    
+			    $url.val(url);
+			    $filename.val(filename);
+			    $content.text(content);
+			    
+			    $save.show();
+			  }
+	);	
 	
 	/*
 	$save.click(function(){
@@ -68,32 +94,7 @@ $(function($){
 	});
 	 */
 	
-	// 接收content script传过来的数据
-	chrome.extension.onRequest.addListener(
-			  function(request, sender, sendResponse) {
-			    console.log(sender.tab ?
-			                "from a content script:" + sender.tab.url :
-			                "from the extension");
-			    if (request.greeting == "hello"){
-			    	sendResponse({farewell: "goodbye"});
-			    }
-			    else{
-			    	sendResponse({}); // snub them.
-			    }
-			      
-			    //chrome.downloads.download(request, function(){});	
-			    
-			    var url = request.url;
-			    var filename = request.filename;
-			    var content = request.content;
-			    
-			    $url.val(url);
-			    $filename.val(filename);
-			    $content.text(content);
-			    
-			    $save.show();
-			  }
-	);
+
 	
 	
 });

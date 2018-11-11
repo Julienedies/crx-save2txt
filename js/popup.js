f66f0f7da7f2f043e26da188c78e7d70a54dfdfa
julienedies <julienedies@gmail.com>
2018-11-11 21:18:51 +0800

    细节优化

--- js/popup.js
+++ js/popup.js
@@ -6,9 +6,41 @@
  *
  */
 
+var data = {};
 
 //向当前html注入js
 window.chrome && chrome.tabs && chrome.tabs.executeScript(null, {file: "js/content_script.js"});
+
+// 接收content script发过来的消息
+chrome.extension.onRequest.addListener(
+    function (request, sender, sendResponse) {
+
+        console.log(sender.tab ?
+        "from a content script:" + sender.tab.url :
+            "from the extension");
+
+        if (request.greeting == "hello") {
+            sendResponse({farewell: "goodbye"});
+        }
+        else {
+            sendResponse({});
+        }
+
+        var url = request.url;
+        var filename = request.filename;
+        var content = request.content;
+
+        data = {title: filename, text: content, url:url};
+
+        $('#url').val(url);
+        $('#filename').val(filename);
+        $('#content').text(content);
+
+    }
+);
+
+
+
 
 $(function ($) {
 
@@ -36,7 +68,7 @@ $(function ($) {
 
         var filename = $filename.val() || (+new Date).toString(36);
 
-        var content = $content.val().replace(/\n/g, '\r\n');
+        var content = $url.val() + '\r\n' + $content.val().replace(/\n/g, '\r\n');
 
         if (!content) return alert('内容为空');
 
@@ -67,31 +99,22 @@ $(function ($) {
 
     });
 
-    // 接收content script发过来的消息
-    chrome.extension.onRequest.addListener(
-        function (request, sender, sendResponse) {
 
-            console.log(sender.tab ?
-            "from a content script:" + sender.tab.url :
-                "from the extension");
-
-            if (request.greeting == "hello") {
-                sendResponse({farewell: "goodbye"});
+    $('#submit').click(function(){
+        $.ajax({
+            url: 'http://localhost:2018/txt',
+            type: 'post',
+            data: {title: $filename.val(), text: data.url + '\r\n' +$content.val(), url:data.url}
+        }).done(function (msg) {
+            chrome.runtime.sendMessage({todo: 'notify', duration: 4, title: '', msg: 'txt保存OK!'});
+        }).fail(function (err) {
+                console.error(err);
+                alert('txt保存出错.');
             }
-            else {
-                sendResponse({});
-            }
+        );
+    });
 
-            var url = request.url;
-            var filename = request.filename;
-            var content = request.content;
 
-            $url.val(url);
-            $filename.val(filename);
-            $content.text(content);
-
-        }
-    );
 
 });
 
